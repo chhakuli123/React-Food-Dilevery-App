15d080b7fae35829ecf7c94b1371c569cd01c932
Chhakuli <chhakulizingare12@gmail.com>
2023-01-27 19:42:18 +0530

    making search functionality

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurentCard from "./RestaurentCard";
 import { restaurantList } from "../Components/config";
 
 function Body() {
+  function filterData(searchText, restaurents) {
+    const filterData = restaurents.filter((restaurent) =>
+      restaurent.data.name.includes(searchText)
+    );
+    return filterData;
+  }
+  const [restaurents, setRestaurents] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <div className="restaurant-list">
-      {restaurantList.map((restaurent) => {
-        return <RestaurentCard {...restaurent.data} />;
-      })}
-    </div>
+    <>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search a restaurent"
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+      />
+      <button
+        className="search-btn"
+        onClick={() => {
+          const data = filterData(searchText, restaurents);
+          setRestaurents(data);
+        }}
+      >
+        Search
+      </button>
+      <div className="restaurant-list">
+        {restaurantList.map((restaurent) => {
+          return (
+            <RestaurentCard {...restaurent.data} key={restaurent.data.id} />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
