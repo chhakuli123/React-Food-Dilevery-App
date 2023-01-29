bf11d13dfd5f5796cad35f6e15fda79ca2dcb790
Chhakuli <chhakulizingare12@gmail.com>
2023-01-29 14:28:20 +0530

    outlet of react dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import Body from "./Components/Body";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Outlet />
       <Footer />
     </div>
   );
