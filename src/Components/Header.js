dbde916865f86af98f6406d594119cf68ed90063
Chhakuli <chhakulizingare12@gmail.com>
2023-01-29 14:29:43 +0530

    Link of react router added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../assets/logo.png";
 function Header() {
@@ -8,9 +9,18 @@ function Header() {
       <img src={logo} alt="" className="logo" />
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About </li>
-          <li>Contact </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/about">About </Link>
+          </li>
+
+          <li>
+            {" "}
+            <Link to="/contact">Contact </Link>
+          </li>
           <li>Cart</li>
           {loggedIn === "true" ? (
             <li>
