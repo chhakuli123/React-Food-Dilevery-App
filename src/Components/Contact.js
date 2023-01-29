ee9d93f14f553ef5130298f44d30e33ac59fdcca
Chhakuli <chhakulizingare12@gmail.com>
2023-01-29 14:29:10 +0530

    contact page added

---
+++ src/Components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Hi there!!!</h1>
+      <h4>This is the contact page</h4>
+    </div>
+  );
+}
+
+export default Contact;
