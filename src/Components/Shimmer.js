3c875c59260a0983bb1acd79e7597537d26c381f
Chhakuli <chhakulizingare12@gmail.com>
2023-02-09 10:51:36 +0530

    [Modification] : Made smimmer animated

--- src/Components/Shimmer.js
+++ src/Components/Shimmer.js
@@ -1,24 +1,28 @@
 import React from "react";
-import shimmer from "../assets/shimmer.png";
-function Shimmer() {
+// import shimmer from "../assets/shimmer.png";
+
+// Shimmer card to display with animation
+const CardShimmer = () => {
   return (
-    <div className=" flex flex-wrap ml-16  ">
-      {Array(15)
-        .fill("")
-        .map((e, index) => (
-          <div
-            className="w-80 p-2 m-3 rounded-md shadow-2xl hover:shadow-lg"
-            key={index}
-          >
-            <img className="rounded-md shadow-md" alt="" src={shimmer} />
-            <h2>Restaurant Name</h2>
-            <h3>Cuisines</h3>
-            <h4>Distance</h4>
-          </div>
-        ))}
-      ;
+    <div className="shimmer-card">
+      <div className="shimmer-img stroke animate"></div>
+      <div className="shimmer-title stroke animate"></div>
+      <div className="shimmer-tags stroke animate "></div>
+      <div className="shimmer-details stroke animate "></div>
     </div>
   );
-}
+};
 
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      {/* create a new Array instance using Array() constructor and map through every element of array */}
+      {Array(15)
+        .fill(" ")
+        .map((element, index) => {
+          return <CardShimmer key={index} />;
+        })}
+    </div>
+  );
+};
 export default Shimmer;
