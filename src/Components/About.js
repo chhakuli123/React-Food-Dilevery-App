0178b008b81b54dd07f1735b7702a84144c09235
Chhakuli <chhakulizingare12@gmail.com>
2023-02-11 08:35:11 +0530

    [Fix] : made about page more attractive

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,23 @@
 import React from "react";
+import RestroMenuDish from "../assets/menu.png";
 
 function About() {
   return (
-    <div>
-      <h1>About us</h1>
-      <p>This is about page</p>
+    <div className="mt-8 ">
+      <img
+        className="content-center h-[23rem] w-[28rem] ml-[33rem]"
+        src={RestroMenuDish}
+        alt=""
+      />
+      <h1 className="text-center font-extrabold text-3xl mb-4 mt-4">
+        Welcome Hungry peoples in the
+      </h1>
+      <h1 className="font-extrabold text-center text-5xl text-red-500">
+        HUNGRAZY
+      </h1>
+      <p className="text-center font-extrabold text-3xl mb-10 mt-4">
+        Which is a food-delivery company, delivers food to a customer with love.
+      </p>
     </div>
   );
 }
