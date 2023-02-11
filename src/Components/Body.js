77f93bdb4a5595ce80d808504016001a5e2f0cb5
Chhakuli <chhakulizingare12@gmail.com>
2023-02-11 08:55:14 +0530

    [Modified] : Logic for no search found page

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import RestaurentCard from "./RestaurentCard";
 import Shimmer from "./Shimmer";
 import useOnline from "../utils/useOnline";
-// import { restaurantList } from "../Components/config";
-
+import sorryNotFound from "../assets/notfound.png";
+import { RESTRO_URL } from "../Components/config";
 function Body() {
   //function to search a restaurent from list,here we are getting all restaurent names
   function filterData(searchText, restaurents) {
@@ -18,6 +18,7 @@ function Body() {
   const [allRestaurents, setAllRestaurents] = useState([]);
   const [filteredRestaurents, setFilteredRestaurents] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   // useEffecthook
   useEffect(() => {
@@ -43,25 +44,41 @@ function Body() {
   // };
 
   //function to fetch the data
+
   async function getRestaurent() {
-    let data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
-    );
+    let data = await fetch(RESTRO_URL);
     let praj = await data.json();
     setAllRestaurents(praj?.data?.cards[2]?.data?.data?.cards);
     setFilteredRestaurents(praj?.data?.cards[2]?.data?.data?.cards);
+    console.log(praj?.data?.cards[2]?.data?.data?.cards);
   }
 
   //hook for user is offline
   const isOnline = useOnline();
   if (!isOnline) {
     return (
-      <span className="font-bold">
+      <span className="font-bold text-center text-red-600">
         Failed to load, Please check your internet connection
       </span>
     );
   }
 
+  // use searchData function and set condition if data is empty show error message
+  const searchData = (searchText, restaurants) => {
+    if (searchText !== "") {
+      const data = filterData(searchText, restaurants);
+      setFilteredRestaurents(data);
+      setErrorMessage("");
+      if (data.length === 0) {
+        setErrorMessage(
+          `Sorry, we couldn't find any results for "${searchText}"`
+        );
+      }
+    } else {
+      setErrorMessage("");
+      setFilteredRestaurents(restaurants);
+    }
+  };
   //we have to put shimmer ui for great user experience
   //if  restaurent is empty =>then load shimmer ui
   //otherwise load api
@@ -72,26 +89,34 @@ function Body() {
   ) : (
     <>
       {/* //search functionality */}
-      <div className=" ml-80 p-4 bg-gray">
+      <div className=" flex justify-center p-4 bg-gray">
         <input
           type="text"
-          className=" ml-72 mx-3 p-1 px-2 border  rounded-lg  border-gray-400 "
-          placeholder="Search a restaurent"
+          className="mx-3 p-1 px-2 w-96 h-11 border rounded-lg  border-gray-400 "
+          placeholder="Search a restaurant you want..."
           value={searchText}
           onChange={(e) => {
             setSearchText(e.target.value);
           }}
         />
         <button
-          className=" p-1 px-2 bg-gray-700 text-white rounded-md"
+          className=" p-1 px-2 w-12 bg-gray-700 text-white rounded-md"
           onClick={() => {
-            const data = filterData(searchText, allRestaurents);
-            setFilteredRestaurents(data);
+            // const data = filterData(searchText, allRestaurents);
+            // setFilteredRestaurents(data);
+            // user click on button searchData function is called
+            searchData(searchText, allRestaurents);
           }}
         >
-          Search
+          <i class="fa-solid fa-magnifying-glass"></i>
         </button>
       </div>
+      {errorMessage && (
+        <div className="flex flex-col justify-center items-center text-4xl font-bold">
+          <img className="h-80 w-80" src={sorryNotFound} alt="" />
+          {errorMessage}
+        </div>
+      )}
 
       {/* we are mapping the every restauraent to erender */}
       <div className="flex flex-wrap ml-12">
