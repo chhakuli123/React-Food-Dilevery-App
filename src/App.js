94784b42d799f5f4a681179f9783e9469efe68da
Chhakuli <chhakulizingare12@gmail.com>
2023-02-24 15:54:55 +0530

    [Fix] : Proveded the store of redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import { Provider } from "react-redux";
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-
+import store from "./Redux_Store/store";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
