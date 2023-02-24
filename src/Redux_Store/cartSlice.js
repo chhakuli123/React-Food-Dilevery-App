52432773d6838f87d6208922d05764ed0dd3f30a
Chhakuli <chhakulizingare12@gmail.com>
2023-02-24 15:58:20 +0530

    [Added] : Cart slice of store

---
+++ src/Redux_Store/cartSlice.js
@@ -0,0 +1,23 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      //this does not return anything it directly takes a state and modifies it
+      state.items.push(action.payload);
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+  },
+});
+
+export const { addItem, clearCart, removeItem } = cartSlice.actions;
+export default cartSlice.reducer;
